8665817e8575a0bf29119bc1ee8f154c7225e0e5
karan17102003 <karanmehra17102003@gmail.com>
2024-06-26 21:25:26 +0530

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+
+ export function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length - 1;
+  
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const guess = arr[mid];
+  
+      if (guess === target) {
+        return mid;
+      } else if (guess < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  
+    return -1; 
+  }
+  
+  module.exports = binarySearch;
+  console.log(binarySearch([1,2,4,5],4));
